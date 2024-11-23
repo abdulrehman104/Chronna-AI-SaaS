b50abbd789095419f81bfb2d47a2d3f4e713a3c6
abdulrehman1004 <rehman.abdul008844@gmail.com>
2024-11-24 00:49:43 +0500

    Complete conversation page & Chat bot functionality

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,10 @@
+import { AiChatBot } from "@/components/chatbot";
+import React from "react";
+
+export default function ChatBot() {
+  return (
+    <div>
+      <AiChatBot />
+    </div>
+  );
+}
